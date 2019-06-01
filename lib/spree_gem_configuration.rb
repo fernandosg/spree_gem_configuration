d5cb07caba9e7dc547a2efb4450f99697c4cabad
fernandosg <fernandosegom@gmail.com>
2019-06-01 00:47:51 -0500

    Se agrega clase para persistir la configuración de la gema en el proceso de definir los modelos a generar el atributo preferences

--- lib/spree_gem_configuration.rb
+++ lib/spree_gem_configuration.rb
@@ -1,4 +1,20 @@
 require 'spree_core'
 require 'spree_extension'
+require 'helpers/custom_configuration_in_gem'
 require 'spree_gem_configuration/engine'
 require 'spree_gem_configuration/version'
+require 'spree_gem_configuration/configuration'
+
+module SpreeGemConfiguration
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+end
---
+++ lib/spree_gem_configuration/configuration.rb
@@ -0,0 +1,9 @@
+module SpreeGemConfiguration
+  class Configuration
+      attr_accessor :models
+
+      def initialize
+        @models = []
+      end
+  end
+end
